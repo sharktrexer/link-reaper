685dd215467fa9b5bedd6580e5a420b97945866f
sharktrexer <sharktrexer@gmail.com>
2024-12-05 17:34:21 -0800

    testt file format

--- link_reaper/tests/test_link_funcs.py
+++ link_reaper/tests/test_link_funcs.py
@@ -2,15 +2,18 @@ import pytest
 
 from link_reaper import link_collector
 
-@pytest.mark.parametrize("str, expected_length", [
-    ("[test](https://test.com)", 1), 
-    ("<https://test.com>", 1),
-    ("[i](i)", 1),
-    ("[invalid(http://test.com)", 0),
-    ("[i]http://test.com)", 0),
-    ("i]http://test.com)", 0),
-    ("http://test.com", 0)
-])
-def test_markdown_link_formats(str, expected_length):
 
+@pytest.mark.parametrize(
+    "str, expected_length",
+    [
+        ("[test](https://test.com)", 1),
+        ("<https://test.com>", 1),
+        ("[i](i)", 1),
+        ("[invalid(http://test.com)", 0),
+        ("[i]http://test.com)", 0),
+        ("i]http://test.com)", 0),
+        ("http://test.com", 0),
+    ],
+)
+def test_markdown_link_formats(str, expected_length):
     assert len(link_collector.grab_md_links(str)) == expected_length
